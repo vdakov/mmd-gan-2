9e73259d3603fb4a4b9e38c79ffe47f9c4b05fcb
vdakov <v.dakov02@gmail.com>
2025-05-30 17:43:32 +0200

    Adjust dataset generation and sizes

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -2,12 +2,15 @@ from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 import torch
 
-def load_CIFAR(save_path="datasets/data", batch_size=64):
-    transform = transforms.Compose([
-        transforms.Resize(64), 
+def load_CIFAR(save_path="datasets/data", batch_size=64, size=64, flatten=False):
+    transform_list = [
+        transforms.Resize(size),  
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
-    ])
+        transforms.Normalize((0.5,), (0.5,))
+    ]
+
+    if flatten:
+        transform_list.append(transforms.Lambda(lambda x: x.view(-1)))
 
     trainset = CIFAR10(root=save_path, train=True, download=True, transform=transform)
     testset = CIFAR10(root=save_path, train=False, download=True, transform=transform)
--- datasets/mnist.py
+++ datasets/mnist.py
@@ -2,12 +2,32 @@ from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 import torch
 
-def load_MNIST(save_path="datasets/data", batch_size=64):
-    transform = transforms.Compose([
-        transforms.Resize(64),  
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+import torch
+
+def load_MNIST(save_path="datasets/data", batch_size=64, size=28, flatten=False):
+    """
+    Loads MNIST dataset with optional flattening.
+
+    Args:
+        save_path (str): Directory where the dataset should be downloaded/loaded from.
+        batch_size (int): Batch size for the DataLoaders.
+        flatten (bool): If True, flattens the 28x28 images into a 1D vector of 784.
+
+    Returns:
+        tuple: (trainloader, testloader)
+    """
+    transform_list = [
+        transforms.Resize(size),  
         transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)) 
-    ])
+        transforms.Normalize((0.5,), (0.5,))
+    ]
+
+    if flatten:
+        transform_list.append(transforms.Lambda(lambda x: x.view(-1)))
+
+    transform = transforms.Compose(transform_list)
 
     trainset = MNIST(root=save_path, train=True, download=True, transform=transform)
     testset = MNIST(root=save_path, train=False, download=True, transform=transform)
@@ -16,3 +36,4 @@ def load_MNIST(save_path="datasets/data", batch_size=64):
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
     return trainloader, testloader
+
